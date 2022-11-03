5d2ecaa0d90d7114b61469605385ff95ee5e02f4
oliviodare <m_bp@posteo.net>
2022-11-03 23:29:12 +0100

    change csv name to download, better csv

--- _site/assets/js/createcsv.js
+++ _site/assets/js/createcsv.js
@@ -110,10 +110,10 @@ function createSelectedCsv() {
     var encodedUri = encodeURI(csvFile);
     var link = document.createElement("a");
     link.setAttribute("href", encodedUri);
-    link.setAttribute("download", "my_sts_list.csv");
+    link.setAttribute("download", "my_HPS_list.csv");
     document.body.appendChild(link); // Required for FF
 
-    link.click(); // This will download the data file named "my_sts_list.csv".
+    link.click(); // This will download the data file named "my_HPS_list.csv".
 
 }
 
